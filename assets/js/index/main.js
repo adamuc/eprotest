cd422cec947e767a880628e7fd920f78b44db533
Adam Mucha <mail@adammucha.eu>
2019-10-18 19:28:20 +0200

    WIP files

--- assets/js/index/main.js
+++ assets/js/index/main.js
@@ -1,4 +1,35 @@
 // CSS
 import '../../css/index/global.scss';
 
-import '../app';
+import $ from '../app';
+
+$(document).ready(() => {
+    $('html, body').scrollTop(0);
+    $('html, body').css({
+        overflow: 'hidden',
+        height: '100%'
+    });
+
+    $('#search').click(() => {
+        // WIP
+        $.ajax({
+            url: '/cms/getClosest',
+            type: 'POST',
+            data: coordinates,
+            success(response) {
+                const r = $.parseJSON(response);
+                $('html, body').css({
+                    overflow: 'auto',
+                    height: 'auto'
+                });
+
+                $('html, body').animate({
+                    scrollTop: $('.result').offset().top
+                }, 1500);
+            },
+            error(errResponse) {
+                console.log(errResponse);
+            }
+        });
+    });
+});
