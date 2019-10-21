a13bc8efd6b3807ef4fa5d449bea448a1aedbe6e
Rafal <kitta.rafal@gmail.com>
2019-10-21 20:32:40 +0200

    Fixed

--- assets/js/forum/main.js
+++ assets/js/forum/main.js
@@ -1,4 +1,11 @@
 // CSS
 import '../../css/forum/global.scss';
+import $ from '../app';
+$(document).ready(() =>{
+    $('.wrapper__user').hover(()=>{
+        $('.wrapper__userOption').fadeIn(300);
+    }, ()=>{
+        $('.wrapper__userOption').fadeOut(300);
+    });
 
-import '../app';
+});
