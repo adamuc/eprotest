db0dfe3801603e1c9263b4191a8869d1a92ca19d
Rafal <kitta.rafal@gmail.com>
2019-10-25 20:29:05 +0200

    Event update

---
+++ assets/js/settings/main.js
@@ -0,0 +1,11 @@
+// CSS
+import '../../css/settings/global.scss';
+import $ from '../app';
+
+$(document).ready(() =>{
+   $('.main__menuItem').click(() =>{
+      $('.main__menuItem').each(() =>{
+          
+      });
+   });
+});
