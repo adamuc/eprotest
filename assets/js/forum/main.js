382b85960d40567e2647a6bf2cd8c25b6e715d6f
Rafal <kitta.rafal@gmail.com>
2019-12-15 22:19:00 +0100

    General update

--- assets/js/forum/main.js
+++ assets/js/forum/main.js
@@ -2,10 +2,47 @@
 import '../../css/forum/global.scss';
 import $ from '../app';
 $(document).ready(() =>{
-    $('.wrapper__user').hover(()=>{
-        $('.wrapper__userOption').fadeIn(300);
-    }, ()=>{
-        $('.wrapper__userOption').fadeOut(300);
-    });
+    function menu() {
+        let click = false;
+        let clickArrow = false;
+        $('.hamburger').click(() =>{
+            if(click == false){
+                $('.navigation__menu').addClass('navigation__menu--active');
+                click = true;
+            }else{
+                $('.navigation__menu').removeClass('navigation__menu--active');
+                click = false;
+            }
+        });
+        $('.arrow').click(() =>{
+            if(clickArrow == false){
+                $('.userMenu').fadeIn(300);
+                $('.userMenu').css('display', 'flex');
+                clickArrow = true;
+            }else{
+                $('.userMenu').fadeOut(300);
+                clickArrow = false;
+            }
+        });
+    }
 
+    function loadContent(index) {
+        $('.loaderBox').fadeIn(250);
+        setTimeout(function () {
+            $('.loadBox').load(index, function () {
+                $('.loaderBox').fadeOut(250);
+            });
+        }, 1000);
+    }
+    function category() {
+        $('.event').click(() =>{
+           let category = $('.event').attr('class').split(' ');
+           switch(category[1]){
+               case 'ecoEvent':
+                   loadContent('eco.html');
+           }
+        });
+    }
+    category();
+    menu();
 });
