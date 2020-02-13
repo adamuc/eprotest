b1ecb55d582f446cfdd42f2ee1e37b269199435f
Adam Mucha <mail@adammucha.eu>
2020-02-13 20:35:07 +0100

    General update (#23) (#24)
    Co-authored-by: Rafał Kita <kitta.rafal@gmail.com>
    
    Co-authored-by: Rafał Kita <kitta.rafal@gmail.com>

--- assets/js/forum/main.js
+++ assets/js/forum/main.js
@@ -2,6 +2,53 @@
 import '../../css/forum/global.scss';
 
 import $ from '../app';
+<<<<<<< HEAD
+$(document).ready(() =>{
+    function menu() {
+        let click = false;
+        let clickArrow = false;
+        $('.hamburger').click(() =>{
+            if(click == false){
+                $('.navigation__menu').addClass('navigation__menu--active');
+                click = true;
+            }else{
+                $('.navigation__menu').removeClass('navigation__menu--active');
+                click = false;
+            }
+        });
+        $('.arrow').click(() =>{
+            if(clickArrow == false){
+                $('.userMenu').fadeIn(300);
+                $('.userMenu').css('display', 'flex');
+                clickArrow = true;
+            }else{
+                $('.userMenu').fadeOut(300);
+                clickArrow = false;
+            }
+        });
+    }
+
+    function loadContent(index) {
+        $('.loaderBox').fadeIn(250);
+        setTimeout(function () {
+            $('.loadBox').load(index, function () {
+                $('.loaderBox').fadeOut(250);
+            });
+        }, 1000);
+    }
+    function category() {
+        $('.event').click(() =>{
+           let category = $('.event').attr('class').split(' ');
+           switch(category[1]){
+               case 'ecoEvent':
+                   loadContent('eco.html');
+           }
+        });
+    }
+    category();
+    menu();
+});
+=======
 
 $(document).ready(() => {
     $('.wrapper__user').hover(() => {
@@ -26,3 +73,4 @@ $(document).ready(() => {
         window.location.href = '/logout';
     });
 });
+>>>>>>> 480d4f9412c999cf3feb028a1db66665e8915488
